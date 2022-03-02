eb41a2f997856dded84f7daa16d9bf83482af6ee
Ray Deck <ray@raydeck.com>
2022-03-02 18:55:44 -0500

    feat: making the interface simpler by removing the release idea

--- tnft-contracts/IERC721Lease.sol
+++ tnft-contracts/IERC721Lease.sol
@@ -15,6 +15,18 @@ interface IERC721Lease {
         uint256 start,
         uint256 end
     );
+    event LeaseApproval(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId,
+        uint256 _start,
+        uint256 _end
+    );
+    event LeaseApprovalForAll(
+        address indexed _from,
+        address indexed _operator,
+        bool _approved
+    );
 
     // event Mint(uint256 indexed tokenId, string tokenURI);
 
@@ -25,24 +37,14 @@ interface IERC721Lease {
 
     function returnBy(uint256 _tokenId) external view returns (uint256);
 
-    function lease(
-        uint256 _tokenId,
-        uint256 _start,
-        uint256 _end
-    ) external payable;
-
-    // For a lease in period ABC, where you sublet period B, you would generate new lease events for A and C for you, and a return event for ABC for you, and a lease event for them for B
-    //emit lease events (split in the original lease, and the new re-lease)
-    //emit return (undo of rights by the original lessee)
-
-    function approveRelease(
+    function approveLease(
         uint256 _tokenId,
         uint256 _start,
         uint256 _end,
         address _addressTo
     ) external;
 
-    function releaseFrom(
+    function leaseFrom(
         address _addressFrom,
         address _addressTo,
         uint256 tokenId,
@@ -50,21 +52,9 @@ interface IERC721Lease {
         uint256 _end
     ) external payable;
 
-    //emit borrow
-    //emit return
-
-    // function uri(uint256 _tokenId) external view returns (string memory);
-
-    // function hash(uint256 _tokenId) external view returns (uint256);
-
-    // function mint(string memory _tokenURI, uint256 hash) external;
-
-    //emit mint
-
     function unlease(
         uint256 _tokenId,
         uint256 _start,
         uint256 _end
     ) external;
-    //emit return
 }
