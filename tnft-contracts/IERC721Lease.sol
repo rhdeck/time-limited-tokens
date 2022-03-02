79879d9c127f2fecbb402c7234bdb3e3f0f2079b
Ray Deck <ray@raydeck.com>
2022-03-01 20:59:07 -0500

    feat: all new ERC721Lease spec is coming together

--- tnft-contracts/IERC721Lease.sol
+++ tnft-contracts/IERC721Lease.sol
@@ -2,28 +2,23 @@
 
 pragma solidity ^0.8.0;
 
-interface ITemporalNFT {
-    event Borrow(
+interface IERC721Lease {
+    event Leased(
         uint256 indexed tokenId,
         address indexed borrower,
         uint256 start,
         uint256 end
     );
-    event Return(
+    event Returned(
         uint256 indexed tokenId,
         address indexed borrower,
         uint256 start,
         uint256 end
     );
-    event Mint(uint256 indexed tokenId, string tokenURI);
 
-    function hasAccess(
-        address _address,
-        uint256 _tokenId,
-        uint256 _block
-    ) external view returns (bool);
+    // event Mint(uint256 indexed tokenId, string tokenURI);
 
-    function borrow(uint256 _tokenId, uint256 _block)
+    function lesseeOf(uint256 _tokenId, uint256 _block)
         external
         view
         returns (address);
@@ -36,38 +31,33 @@ interface ITemporalNFT {
         uint256 _end
     ) external payable;
 
-    function transfer(
-        uint256 _tokenId,
-        uint256 _start,
-        uint256 _end,
-        address _addressTo
-    ) external payable;
+    // For a lease in period ABC, where you sublet period B, you would generate new lease events for A and C for you, and a return event for ABC for you, and a lease event for them for B
+    //emit lease events (split in the original lease, and the new re-lease)
+    //emit return (undo of rights by the original lessee)
 
-    //emit borrow
-    //emit return
-
-    function approve(
+    function approveRelease(
         uint256 _tokenId,
         uint256 _start,
         uint256 _end,
         address _addressTo
     ) external;
 
-    function transferFrom(
+    function releaseFrom(
+        address _addressFrom,
+        address _addressTo,
         uint256 tokenId,
         uint256 _start,
-        uint256 _end,
-        address _addressFrom
+        uint256 _end
     ) external payable;
 
     //emit borrow
     //emit return
 
-    function uri(uint256 _tokenId) external view returns (string memory);
+    // function uri(uint256 _tokenId) external view returns (string memory);
 
-    function hash(uint256 _tokenId) external view returns (uint256);
+    // function hash(uint256 _tokenId) external view returns (uint256);
 
-    function mint(string memory _tokenURI, uint256 hash) external;
+    // function mint(string memory _tokenURI, uint256 hash) external;
 
     //emit mint
 
