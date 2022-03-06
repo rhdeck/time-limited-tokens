3cb4c01f8c88693d79a5450c78593d8161919828
Ray Deck <ray@raydeck.com>
2022-03-06 16:13:16 -0500

    feat: fixing with etherizer

--- roberts/lease/components/Etherizer.js
+++ roberts/lease/components/Etherizer.js
@@ -7,7 +7,7 @@ import React, {
   useMemo,
 } from "react";
 import Web3Modal from "web3modal";
-import ethers from "ethers";
+import { ethers } from "ethers";
 const context = createContext({});
 const { Provider } = context;
 export const useEtherizer = () => {
@@ -19,7 +19,7 @@ const Etherizer = ({ children }) => {
   const [isConnected, setIsConnected] = useState(false);
   const [isConnecting, setIsConnecting] = useState(false);
   const [provider, setProvider] = useState();
-  const [signer, getSigner] = useState();
+  const [signer, setSigner] = useState();
   useEffect(() => {
     (async () => {
       console.log("Window is ", window);
@@ -28,9 +28,9 @@ const Etherizer = ({ children }) => {
         setHasEthereum(true);
         if (window.ethereum.isConnected()) {
           console.log("I is connected");
-
           const accounts = await ethereum.request({ method: "eth_accounts" });
           if (accounts && accounts.length !== 0) {
+            console.log("I have accounts");
             setIsConnected(true);
           }
         }
@@ -60,18 +60,28 @@ const Etherizer = ({ children }) => {
         });
 
         const instance = await web3Modal.connect();
-        setProvider(new ethers.providers.Web3Provider(instance));
-        setSigner(provider.getSigner());
+        console.log("I got connected from the connect call", instance);
+        const p = new ethers.providers.Web3Provider(instance);
+        setProvider(p);
+        console.log("I have a provider now", p);
+        setSigner(p.getSigner());
+        console.log("I hhave a signer now");
         // await window.ethereum.send("eth_requestAccounts");
-        setIsConnected(window.ethereum.isConnected());
-      } catch (error) {}
+        setIsConnected(true);
+      } catch (error) {
+        console.log("This is not work at all!!!!!", error);
+      }
       setIsConnecting(false);
     })();
   }, []);
-
+  const disconnect = useCallback(async () => {
+    console.log("NOOOOOOOOOOO");
+    await window.web3.eth.currentProvider.disconnect();
+    setIsConnected(false);
+  }, [provider]);
   const value = useMemo(() => {
-    return { isConnected, provider };
-  }, [isConnected, provider, signer]);
+    return { isConnected, provider, signer, disconnect };
+  }, [isConnected, provider, signer, disconnect]);
   if (!hasEthereum) {
     return "No wallet software detected";
   } else if (!isConnected) {
