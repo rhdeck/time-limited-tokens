6793f4e96cedea647c0c8967bfcc103825a7b254
Ray Deck <ray@raydeck.com>
2022-03-06 15:42:29 -0500

    refactor: Moving Etherizer out of _app

---
+++ roberts/lease/components/Etherizer.js
@@ -0,0 +1,91 @@
+import React, {
+  useEffect,
+  useCallback,
+  useState,
+  useContext,
+  createContext,
+  useMemo,
+} from "react";
+import Web3Modal from "web3modal";
+import ethers from "ethers";
+const context = createContext({});
+const { Provider } = context;
+export const useEtherizer = () => {
+  const { isConnected } = useContext(context);
+  return { isConnected };
+};
+const Etherizer = ({ children }) => {
+  const [hasEthereum, setHasEthereum] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [provider, setProvider] = useState();
+  const [signer, getSigner] = useState();
+  useEffect(() => {
+    console.log("Window is ", window);
+    if (window && window.ethereum) {
+      console.log("I am so happy", window.ethereum);
+      setHasEthereum(true);
+      if (window.ethereum.isConnected()) {
+        console.log("I is connected");
+        setIsConnected(true);
+      }
+    }
+  }, []);
+  const providerOptions = {
+    injected: {
+      display: {
+        logo: "data:image/gif;base64,INSERT_BASE64_STRING",
+        name: "Injected",
+        description: "Connect with the provider in your Browser",
+      },
+      package: null,
+    },
+  };
+  const network = "mumbai";
+  const connect = useCallback(() => {
+    (async () => {
+      console.log("Running requestaccounts");
+      setIsConnecting(true);
+      try {
+        const web3Modal = new Web3Modal({
+          network: "mainnet", // optional
+          cacheProvider: true, // optional
+          providerOptions, // required
+        });
+
+        const instance = await web3Modal.connect();
+        setProvider(new ethers.providers.Web3Provider(instance));
+        setSigner(provider.getSigner());
+        // await window.ethereum.send("eth_requestAccounts");
+        setIsConnected(window.ethereum.isConnected());
+      } catch (error) {}
+      setIsConnecting(false);
+    })();
+  }, []);
+
+  const value = useMemo(() => {
+    return { isConnected, provider };
+  }, [isConnected, provider, signer]);
+  if (!hasEthereum) {
+    return "No wallet software detected";
+  } else if (!isConnected) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          width: "100vw",
+          height: "100vw",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button onClick={connect} className="button" disabled={isConnecting}>
+          {isConnecting ? "Connecting..." : "Connect to Mumbai"}
+        </button>
+      </div>
+    );
+  } else {
+    return <Provider value={value}>{children}</Provider>;
+  }
+};
+export default Etherizer;
--- roberts/lease/pages/_app.js
+++ roberts/lease/pages/_app.js
@@ -1,70 +1,7 @@
-import React, {
-  useEffect,
-  useCallback,
-  useState,
-  useContext,
-  createContext,
-  useMemo,
-} from "react";
 import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout/layout.js";
-
-const context = createContext({});
-const { Provider } = context;
-const Etherizer = ({ children }) => {
-  const [hasEthereum, setHasEthereum] = useState(false);
-  const [isConnected, setIsConnected] = useState(false);
-  const [isConnecting, setIsConnecting] = useState(false);
-  useEffect(() => {
-    console.log("Window is ", window);
-    if (window && window.ethereum) {
-      console.log("I am so happy", window.ethereum);
-      setHasEthereum(true);
-      // if (window.ethereum.isConnected()) {
-      //   console.log(window.ethereum.)
-      //   setIsConnected(true);
-      // }
-    }
-  }, []);
-  const connect = useCallback(() => {
-    (async () => {
-      console.log("Running requestaccounts");
-      setIsConnecting(true);
-      try {
-        await window.ethereum.send("eth_requestAccounts");
-        setIsConnected(window.ethereum.isConnected());
-      } catch (error) {}
-      setIsConnecting(false);
-    })();
-  }, []);
-
-  const value = useMemo(() => {
-    return { isConnected };
-  }, [isConnected]);
-  if (!hasEthereum) {
-    return "No wallet software detected";
-  } else if (!isConnected) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          width: "100vw",
-          height: "100vw",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <button onClick={connect} className="button" disabled={isConnecting}>
-          {isConnecting ? "Connecting..." : "Connect to Mumbai"}
-        </button>
-      </div>
-    );
-  } else {
-    return <Provider value={value}>{children}</Provider>;
-  }
-};
-
+import Etherizer from "../components/Etherizer";
 function MyApp({ Component, pageProps }) {
   return (
     <Etherizer>
